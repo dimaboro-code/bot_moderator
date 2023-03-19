1909e24bb43a26d394fd5d2f4573bbfcc83726b2
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-03-19 20:31:23 +0600

    add things for Heroku

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ db = Database('database.db')
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
