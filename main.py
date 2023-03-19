d829118afcf2573e36dcfba241c31622604a5eb9
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-03-19 21:42:21 +0600

    add things for Heroku

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import logging
-import config
-from db import Database
-
-from aiogram import Bot, Dispatcher, executor, types
+import os
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
+from databases import Database
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
-db = Database('database.db')
-bot = Bot(token=config.token)
-dp = Dispatcher(bot)
-
+# Initialize vars
+TOKEN = os.getenv('BOT_TOKEN')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# Initialize bot and dispatcher
+db = Database('sqlite:///db.db')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
@@ -30,6 +33,7 @@ async def on_startup(dispatcher):
 
 async def on_shutdown(dispatcher):
     await bot.delete_webhook()
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -100,4 +104,13 @@ async def text(message: types.Message):
         await message.delete()
 
 if __name__ == '__main__':
-   executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
