d11359c86636b29ace948e4f5ab2a537c41dd621
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-04-01 03:13:36 +0600

    add database

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ async def remove_from_mute(user_id):
         values={'user_id': user_id}
     )
     user_data = [res.values() for res in results]
-    return [str(x) for x in user_data]
+    return str(user_data)
 
 # private chat functions
 @dp.message_handler(commands=['start', 'help'], chat_type='private')
