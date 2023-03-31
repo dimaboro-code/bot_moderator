80653ef8e87b3e1c5cb311e7ab5f8833e32d2b0b
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-04-01 03:08:27 +0600

    add database

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def remove_from_mute(user_id):
         f'SELECT MAX (id) FROM mutes WHERE user_id = :user_id)',
         values={'user_id': user_id}
     )
-    user_data = [res.values() for res in results]
+    user_data = [[x for x in res.values()] for res in results]
     return user_data
 
 # private chat functions
