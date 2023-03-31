698f63dca59a1bdddf3ede6a3d6b460a269af46d
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-03-31 23:55:01 +0600

    add things for Heroku, disable database

--- db.py
+++ db.py
@@ -1,33 +1,34 @@
-import sqlite3
 
 
-class Database:
-    def __init__(self, db_file):
-        self.connection = sqlite3.connect(db_file)
-        self.cursor = self.connection.cursor()
 
-    def exist_user(self, user_id):
-        with self.connection:
-            res = self.cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id, )).fetchall()
-            return bool(len(res))
-
-    def add(self, user_id):
-        with self.connection:
-            return self.connection.execute("INSERT INTO users ('user_id') VALUES (?)", (user_id,))
-
-    def mute(self, user_id):
-        with self.connection:
-            user = self.connection.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)).fetchone()
-            return user[5] == 1
-
-    def add_mute(self, user_id, is_mute=1):
-        return self.connection.execute("UPDATE users SET moderator_username = ? WHERE user_id = ?", (is_mute, user_id))
-
-    def unmute(self, user_id, is_muted=''):
-        with self.connection:
-            lifes = self.cursor.execute("SELECT lifes FROM users WHERE user_id = ?", (user_id,)).fetchone()
-            print(lifes[0])
-        return (
-            (self.connection.execute("UPDATE users SET is_muted = ? WHERE user_id = ?", (is_muted, user_id))),
-            (self.connection.execute("UPDATE users SET lifes = ? WHERE user_id = ?", (lifes[0]-1, user_id)))
-        )
+# class Database:
+#     def __init__(self, db_file):
+#         self.connection = sqlite3.connect(db_file)
+#         self.cursor = self.connection.cursor()
+#
+#     def exist_user(self, user_id):
+#         with self.connection:
+#             res = self.cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id, )).fetchall()
+#             return bool(len(res))
+#
+#     def add(self, user_id):
+#         with self.connection:
+#             return self.connection.execute("INSERT INTO users ('user_id') VALUES (?)", (user_id,))
+#
+#     def mute(self, user_id):
+#         with self.connection:
+#             user = self.connection.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)).fetchone()
+#             return user[5] == 1
+#
+#     def add_mute(self, user_id, is_mute=1):
+#         return self.connection.execute("UPDATE users SET moderator_username = ? WHERE user_id = ?",
+#         (is_mute, user_id))
+#
+#     def unmute(self, user_id, is_muted=''):
+#         with self.connection:
+#             lifes = self.cursor.execute("SELECT lifes FROM users WHERE user_id = ?", (user_id,)).fetchone()
+#             print(lifes[0])
+#         return (
+#             (self.connection.execute("UPDATE users SET is_muted = ? WHERE user_id = ?", (is_muted, user_id))),
+#             (self.connection.execute("UPDATE users SET lifes = ? WHERE user_id = ?", (lifes[0]-1, user_id)))
+#         )
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import asyncio
 import logging
 import os
+from contextlib import suppress
 from aiogram.dispatcher import Dispatcher
+from aiogram.utils.exceptions import (MessageCantBeDeleted, MessageToDeleteNotFound)
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
+from databases import Database
+import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -10,10 +15,12 @@ logging.basicConfig(level=logging.INFO)
 # Initialize vars
 TOKEN = os.getenv('BOT_TOKEN')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-# Initialize bot and dispatcher
+# Initialize bot, database and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+database = Database(DATABASE_URL)
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
@@ -25,51 +32,75 @@ WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
-async def on_startup(dispatcher):
+# webhook control
+async def on_startup():
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    await database.connect()
 
-
-async def on_shutdown(dispatcher):
+async def on_shutdown():
+    await database.disconnect()
     await bot.delete_webhook()
 
 
-@dp.message_handler(commands=['start', 'help'])
+# help_functions
+async def delete_message(message: types.Message, sleep_time: int = 0):
+    await asyncio.sleep(sleep_time)
+    with suppress(MessageCantBeDeleted, MessageToDeleteNotFound):
+        await message.delete()
+
+# database functions
+async def in_database(user_id):
+    results = await database.fetch_all(f'SELECT * FROM users '
+                                       f'WHERE user_id = :user_id ',
+                                       values={'user_id': user_id})
+    return bool(len(results))
+
+async def add_user(message: types.Message):
+    date = str(datetime.datetime.now())[:19]
+    await database.execute(f'INSERT INTO users (user_id, date_add) '
+                           f'VALUES (:user_id, :date_add)',
+                           values={'user_id': message.reply_to_message.from_user.id,
+                                   'date_add': date})
+
+async def add_mute(mute_data):
+    await database.execute(f'INSERT INTO mutes (user_id, message_id, chat_id, '
+                           f'moderator_message, date_of_mute, admin_username) '
+                           f'VALUES (:user_id, :message_id, :chat_id, '
+                           f':moderator_message, :date_of_mute, :admin_username)',
+                           values=mute_data)
+    user_id = mute_data['user_id']
+    lives = await database.fetch_one(f'SELECT user_blocks FROM users WHERE user_id = :user_id',
+                             values={'user_id': user_id})
+    await database.execute(f'UPDATE users SET (user_block = :user_block, is_muted = :is_muted)'
+                           f'WHERE user_id = :user_id',
+                           values={'user_blocks': int(lives) - 1, 'user_id': user_id, 'is_muted': 'TRUE'})
+
+async def remove_from_mute(user_id):
+    results = await database.fetch_all(
+        f'SELECT * FROM mutes WHERE user_id = :user_id AND date_of_mute = ('
+        f'SELECT MAX (date_of_mute) FROM mutes WHERE user_id = :user_id)',
+        values={'user_id': user_id}
+    )
+    user_data = [next(result.values()) for result in results]
+    return user_data
+
+# private chat functions
+@dp.message_handler(commands=['start', 'help'], chat_type='private')
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.answer(f'Hello, {message.from_user.first_name}\n {message}')
+    await message.answer(f'Hello, {message.from_user.first_name}')
 
 
-@dp.message_handler(commands=['mute'], is_chat_admin=True)
-async def mute(message: types.Message):
-    if not message.reply_to_message:
-        await message.reply('Команда должна быть ответом на сообщение!')
-
-    user_message = message.reply_to_message
-    # user_id = message.reply_to_message.from_user.id
-    # user_username = message.reply_to_message.from_user.username
-    # moderator_username = message.from_user.username
-    # db.add_mute(user_id)
-    new = types.ChatPermissions(
-        can_send_messages=False,
-        can_send_media_messages=False,
-        can_send_other_messages=False,
-        can_add_web_page_previews=False
-    )
-    await bot.restrict_chat_member(
-        message.chat.id,
-        user_message.from_user.id,
-        permissions=new
-    )
-    await message.answer(message.chat.id)
-
-
-@dp.message_handler(chat_type='private', commands=['unmute'])
+@dp.message_handler(chat_type='private', commands=['unmute'], commands_prefix='!/')
 async def unmute(message: types.Message):
-    
     user_id = message.from_user.id
-    new = types.ChatPermissions(
+
+    if not in_database(user_id):
+        return message.answer('Вы вне системы. Совершите противоправное действие, чтобы присоединиться')
+
+    user_data = await remove_from_mute(user_id)
+    await message.answer(user_data)
+
+    unmute_hammer = types.ChatPermissions(
         can_send_messages=True,
         can_send_media_messages=True,
         can_send_other_messages=True,
@@ -77,15 +108,70 @@ async def unmute(message: types.Message):
     )
     await bot.restrict_chat_member(
         message.text[8:],  # chat id
-        message.from_user.id,
-        permissions=new
+        user_id,
+        permissions=unmute_hammer
     )
     await message.answer('done')
 
 
+# group chat functions
+@dp.message_handler(commands=['mute'], is_chat_admin=True, commands_prefix='!/')
+async def mute(message: types.Message):
+    # checking form to be right
+    if not message.reply_to_message:
+        tmp = await message.reply('Команда должна быть ответом на сообщение!', )
+        asyncio.create_task(delete_message(tmp, 10))
+
+    # data added to db
+    mute_data = {
+        'chat_id': message.chat.id,
+        'user_id': message.reply_to_message.from_user.id,
+        'message_id': message.reply_to_message.message_id,
+        'moderator_message': message.text[5:],
+        'date_on_mute': str(datetime.datetime.now()[:18]),
+        'admin_username': message.from_user.username
+    }
+    # add user to database
+    if not in_database(mute_data['user_id']):
+        await add_user(message)
+    # add mute to database
+    await add_mute(mute_data)
+
+    # set permissions to forbidden
+    mute_hummer = types.ChatPermissions(
+        can_send_messages=False,
+        can_send_media_messages=False,
+        can_send_other_messages=False,
+        can_add_web_page_previews=False
+    )
+    # change permissions
+    await bot.restrict_chat_member(
+        message.chat.id,
+        message.reply_to_message.from_user.id,
+        permissions=mute_hummer
+    )
+    tmp = await message.answer(message.chat.id)
+    asyncio.create_task(delete_message(tmp, 10))
+    await message.delete()
+
+
 # delete messages about join and left group
-@dp.message_handler(content_types=['new_chat_members', 'left_chat_member'])
-async def new_chat(message: types.Message):
+
+messages_for_delete = [
+    'new_chat_members',
+    'left_chat_member',
+    'delete_chat_photo',
+    'delete_chat_sticker_set',
+    'delete_chat_title',
+    'pinned_message',
+    'unpinned_message',
+    'new_chat_title',
+    'new_chat_description',
+]
+
+
+@dp.message_handler(content_types=messages_for_delete)
+async def delete_messages(message: types.Message):
     await message.delete()
 
 
