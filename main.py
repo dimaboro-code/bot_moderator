8671255be76f57d3c51e403c4afe35b20faaf5bf
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-04-01 02:41:06 +0600

    add database

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def add_mute(mute_data):
                              values={'user_id': user_id})
     lives = str(int(lives[0]) - 1)
     await bot.send_message(mute_data['chat_id'], lives)
-    await database.execute(f'UPDATE users SET user_blocks = :user_blocks'
+    await database.execute(f'UPDATE users SET user_blocks = :user_blocks, is_muted = TRUE '
                            f'WHERE user_id = :user_id',
                            values={'user_blocks': lives, 'user_id': user_id})
 
