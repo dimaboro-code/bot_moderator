fcd4f34a59bf32df119acca10e1e91bdefb84427
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-04-23 23:34:56 +0600

    make prod version

--- main.py
+++ main.py
@@ -216,23 +216,23 @@ async def delete_messages(message: types.Message):
     await message.delete()
 
 
-async def startup(dp):
-    await database.connect()
-
-
-async def shutdown(dp):
-    await database.disconnect()
+# async def startup(dp):
+#     await database.connect()
+#
+#
+# async def shutdown(dp):
+#     await database.disconnect()
 
 
 if __name__ == '__main__':
 
-    start_polling(dp, skip_updates=True, on_startup=startup, on_shutdown=shutdown)
-    # start_webhook(
-    #     dispatcher=dp,
-    #     webhook_path=WEBHOOK_PATH,
-    #     skip_updates=True,
-    #     on_startup=on_startup,
-    #     on_shutdown=on_shutdown,
-    #     host=WEBAPP_HOST,
-    #     port=WEBAPP_PORT,
-    # )
+    # start_polling(dp, skip_updates=True, on_startup=startup, on_shutdown=shutdown)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
