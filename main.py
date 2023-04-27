750a5c356c581cb4cf2b2fa2292b02506398cc96
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-04-27 16:28:00 +0600

    Improve hello message

--- main.py
+++ main.py
@@ -278,13 +278,13 @@ async def shutdown(dp):
 
 if __name__ == '__main__':
 
-    start_polling(dp, skip_updates=True, on_startup=startup, on_shutdown=shutdown)
-    # start_webhook(
-    #     dispatcher=dp,
-    #     webhook_path=WEBHOOK_PATH,
-    #     skip_updates=True,
-    #     on_startup=on_startup,
-    #     on_shutdown=on_shutdown,
-    #     host=WEBAPP_HOST,
-    #     port=WEBAPP_PORT,
-    # )
+    # start_polling(dp, skip_updates=True, on_startup=startup, on_shutdown=shutdown)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
