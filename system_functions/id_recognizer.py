60f6b6051185c43f4ce5e34ccac9f84d18aea862
dimaboro-code <79355964+dimaboro-code@users.noreply.github.com>
2023-07-29 21:18:10 +0600

    add check to know_id

--- system_functions/id_recognizer.py
+++ system_functions/id_recognizer.py
@@ -1,16 +1,24 @@
 from aiogram import types
-from db import get_id, add_id
+from db import get_id, add_id, check_know_id
 
 
 async def know_id(message: types.Message):
     username = message.from_user.username
     user_id = message.from_user.id
 
-    try:
-        if await get_id(username) is not None:
-            return
+    if username is None:
+        print('Нет юзернейма в сообщении, id', user_id)
+        print(message)
+        return
 
+    if await get_id(username) is not None:
+        print('id найден, юзернейм ', username)
+        return
+
+    for i in range(1, 6):
         await add_id(username=username, user_id=user_id)
-
-    except Exception as e:
-        print(f"Произошла ошибка: {str(e)}")
+        check_username = await check_know_id(user_id)
+        if check_username is None:
+            print(f'Нет юзернейма в базе данных, попытка {i}')
+            continue
+        break
