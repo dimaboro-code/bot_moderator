2a4caa22454d85c1447d14184890ca745f5be23d
Dmitry <79355964+dimaboro-code@users.noreply.github.com>
2023-12-25 22:22:26 +0600

    Revert "Повтор предыдущего, проблема была в неправильном URI для базы данных (#41)" (#42)
    This reverts commit f70f9ba32b60b770233a294d1e6bcbdd90f378d0.

---
+++ config.py
@@ -0,0 +1,77 @@
+# python standard library
+import os
+
+# import framework
+from aiogram import Bot, Dispatcher
+
+
+from aiogram import types
+
+# init bot and dispatcher
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+CHATS = [
+    -1001302438185,  # figmachat
+    -1001808148145,  # figmaforum
+    -1001398488197,  # designchat2
+    -1001535946932,  # systemschat
+    -1001124768091,  # framerchat
+    -1001753295642,  # whatthefontt
+    -1001191920744,  # slashcomments
+    -1001769444523,  # slashimagineai
+    -1001838011289,  # Bot Sandbox
+    -1001629596705,  # uireview
+]
+
+LOG_CHANNEL = -1001482081082  # /designer/mutes
+
+MESSAGES_FOR_DELETE = [
+    'new_chat_members',
+    'left_chat_member',
+    'delete_chat_photo',
+    'delete_chat_sticker_set',
+    'delete_chat_title',
+    'pinned_message',
+    'unpinned_message',
+    'new_chat_title',
+    'new_chat_description',
+]
+
+MUTE_SETTINGS = types.ChatPermissions(
+    can_send_messages=False,
+    can_send_media_messages=False,
+    can_send_other_messages=False,
+    can_add_web_page_previews=False,
+)
+
+UNMUTE_SETTINGS = types.ChatPermissions(
+    can_send_messages=True,
+
+    # photo, video, stickers
+    can_send_media_messages=True,
+
+    # files
+    can_send_other_messages=True,
+
+    can_send_polls=True,
+
+    # links
+    can_add_web_page_previews=True
+)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+DATABASE_URL = os.getenv('DATABASE_URL') + '?sslmode=require'
---
+++ system_functions/callback_show_users.py
@@ -0,0 +1,46 @@
+from aiogram.types import CallbackQuery
+from config import bot
+from db import add_lives, delete_lives, delete_all_lives, get_id, get_last_mute, get_user
+from system_functions.is_username import is_username
+from system_functions.show_user_keyboard import show_user_keyboard
+
+
+react_funcs = {
+    'add_unblock': add_lives,
+    'remove_unblock': delete_lives,
+    'remove_all_unblocks': delete_all_lives
+}
+
+
+async def show_user_react(call: CallbackQuery):
+    func_query = call.data.split('_')
+    real_query = '_'.join(func_query[2:])
+    print(real_query)
+    username = await is_username(call.message.text)
+    print(username)
+    user_id = await get_id(username)
+    print(user_id)
+
+    await react_funcs[real_query](user_id)
+    user_status = await get_user(user_id)
+    user_last_mute = await get_last_mute(user_id)
+    chat = await bot.get_chat(user_last_mute["chat_id"])
+
+    await call.answer(show_alert=False, text='Успешно')
+
+    updated_text = (
+        f'Пользователь: @{username}\n'
+        f'Статус: {("без ограничений", "в мьюте")[user_status["is_muted"]]}\n'  # 
+        f'Осталось разблоков: {user_status["user_blocks"]}\n\n'
+        f'Последний мьют\n'
+        f'Причина: {user_last_mute["moderator_message"]}\n'
+        f'Чат: @{chat.username}\n'
+        f'Админ: @{user_last_mute["admin_username"]}\n'
+        f'Дата мьюта: {user_last_mute["date_of_mute"]}'
+    )
+    print(updated_text)
+    await bot.edit_message_text(
+        chat_id=call.message.chat.id, message_id=call.message.message_id,
+        text=updated_text,
+        reply_markup=show_user_keyboard
+    )
