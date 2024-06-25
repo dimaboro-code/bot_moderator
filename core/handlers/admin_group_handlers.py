ea2c974ed8e93865493aec40840c56419c5b05d9
dimaboro-code <dimka.borovikov@gmail.com>
2024-06-25 22:22:33 +0500

    add ban untested

--- core/handlers/admin_group_handlers.py
+++ core/handlers/admin_group_handlers.py
@@ -1,12 +1,16 @@
-from aiogram import Router, Bot, types
+import json
+
+from aiogram import Router, Bot, types, F
 from aiogram.filters import Command
 
 from core.database_functions.db_functions import add_lives
 from core.filters.admin_filter import AdminFilter
 from core.models.data_models import UserData
+from core.redis import rclient
 from core.services.mute import mute
 from core.utils.delete_message_with_delay import delete_message
 from core.utils.text_checks import checks
+from core.config_vars import ConfigVars
 
 admin_group_router = Router()
 admin_group_router.message.filter(AdminFilter())
@@ -59,3 +63,57 @@ async def add_unblocks_handler(message: types.Message, session):
     lives = int(message.text[14:]) if len(str(message.text)) >= 15 else 1
     await add_lives(user_id, session, lives)
     await message.delete()
+
+
+@admin_group_router.message(Command(commands='ban'), filter=F.reply_to_message is not None)
+async def ban_reply_handler(moderator_message: types.Message, bot: Bot):
+    user_to_ban = moderator_message.reply_to_message.from_user.id
+    for chat in ConfigVars.CHATS:
+        success = await bot.ban_chat_member(chat_id=chat, user_id=user_to_ban, until_date=10, revoke_messages=True)
+        if not success:
+            with open("ban.log", "a") as f:
+                f.write(f'Бан не прошел. Пользователь {user_to_ban}, чат {chat}\n')
+    success_message = await moderator_message.answer(
+        f'Пользователь попал в бан. Отменить данное действие возможно только вручную '
+    )
+    try:
+        await bot.delete_message(
+            chat_id=moderator_message.chat.id,
+            message_id=moderator_message.reply_to_message.message_id
+        )
+    except Exception as e:
+        print('Сообщение с нарушением не удалено, ошибка:', e)
+
+    await delete_message(moderator_message)
+    await delete_message(success_message, 1)
+
+
+@admin_group_router.message(Command(commands='ban'), filter=F.reply_to_message is None)
+async def ban_name_handler(moderator_message: types.Message, bot: Bot):
+    name = moderator_message.text.split()[1]
+    print(name)
+    user_data_encoded = rclient.get(name=name)
+    if not user_data_encoded:
+        msg = await moderator_message.answer(f'Пользователь {name} не найден')
+        await delete_message(msg, 1)
+        return
+    user_to_ban = json.loads(user_data_encoded)['tg_id']
+
+    for chat in ConfigVars.CHATS:
+        success = await bot.ban_chat_member(chat_id=chat, user_id=user_to_ban, until_date=10, revoke_messages=True)
+        if not success:
+            with open("ban.log", "a") as f:
+                f.write(f'Бан не прошел. Пользователь {user_to_ban}, чат {chat}\n')
+    success_message = await moderator_message.answer(
+        f'Пользователь попал в бан. Отменить данное действие возможно только вручную '
+    )
+    try:
+        await bot.delete_message(
+            chat_id=moderator_message.chat.id,
+            message_id=moderator_message.reply_to_message.message_id
+        )
+    except Exception as e:
+        print('Сообщение с нарушением не удалено, ошибка:', e)
+
+    await delete_message(moderator_message)
+    await delete_message(success_message, 1)
--- core/middlewares/config_mw.py
+++ core/middlewares/config_mw.py
@@ -1,10 +1,11 @@
+import json
+from datetime import datetime, timedelta
 from typing import Dict, Any, Callable, Awaitable
 from aiogram import BaseMiddleware
+from aiogram.enums import ChatAction
 from aiogram.types import TelegramObject
 from aiogram.exceptions import TelegramBadRequest
-
-from core.utils.add_user_to_db import add_user_to_db
-
+from ..redis import rclient
 
 # отсюда нужно создавать сессию бд, которую потом пробрасывать дальше
 class ConfigMiddleware(BaseMiddleware):
@@ -18,16 +19,34 @@ class ConfigMiddleware(BaseMiddleware):
         data: Dict[str, Any]
     ) -> Any:
         async with self.session() as session:
-            if event.message:
-                await add_user_to_db(event.message, session)
+            try:
+                if event.message is not None:
+                    if event.message.from_user.username:
+                        name = event.message.from_user.username
+                    elif event.message.from_user.last_name:
+                        name = event.message.from_user.first_name + ' ' + event.message.from_user.last_name
+                    else:
+                        name = event.message.from_user.first_name
+                    text = event.message.text
+                    tg_id = event.message.from_user.id
+                    time = datetime.now()
+                    data = rclient.get(tg_id)
+                    if data is None:
+                        data = {'text': text,
+                                'tg_id': tg_id,
+                                'name': name,
+                                'time': time}
+                        ex_time = time+timedelta(days=1)
+                        rclient.set(tg_id, json.dumps(data), exat=ex_time.timestamp())
+                        rclient.set(name, json.dumps(data), exat=ex_time.timestamp())
+                else:
+                    pass
+            except Exception as e:
+                print('no redis')
             data['session'] = session
-            for _ in range(3):
-                try:
-                    await handler(event, data)
-                    break
-                except TelegramBadRequest as e:
-                    print('бэд рекуест, ошибка ', e)
-                    continue
-                except Exception as e:
-                    print(f'Не работает, ошибка: {e}')
+            try:
+                await handler(event, data)
+            except TelegramBadRequest as e:
+                with open('errors.log', 'a') as f:
+                    f.write(f'Ошибка: {e} - запрос: {event.model_dump_json()}\n')
         return
