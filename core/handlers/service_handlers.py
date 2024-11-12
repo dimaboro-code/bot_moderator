726a612facbe93cbb6afe70ffd7aff7072b0cb4a
dimaboro-code <dimka.borovikov@gmail.com>
2024-11-12 11:49:09 +0500

    create capcha handler
    all new chat members will muted if enabled

--- core/handlers/service_handlers.py
+++ core/handlers/service_handlers.py
@@ -1,11 +1,33 @@
-from aiogram import Router, F
+from aiogram import Router, F, Bot
+from aiogram.enums import ContentType
 from aiogram.types import Message
 
 from core import ConfigVars
+from core.database_functions.db_functions import get_last_mute, add_lives
+from core.filters.filters import CapchaChatFilter
+from core.models.data_models import UserData
 from core.services.join_cleaner import join_cleaner
+from core.services.mute import mute
 
 service_router = Router()
 service_router.message.filter(F.content_type.in_(ConfigVars.MESSAGES_FOR_DELETE))
+
+
+@service_router.message(CapchaChatFilter(), F.content_type == ContentType.NEW_CHAT_MEMBERS)
+async def capcha_handler(message: Message, bot: Bot, admins: list):
+    user_id = message.from_user.id
+    if user_id in admins:
+        return
+    last_mute = await get_last_mute(user_id)
+    if last_mute:
+        # не новый пользователь
+        return
+    data = UserData()
+    data.parse_message(message, user_id=user_id)
+    data.chat_id = message.chat.id
+    data.reason_message = 'capcha'
+    await add_lives(user_id)
+    await mute(data=data, bot=bot)
 
 
 @service_router.message()
